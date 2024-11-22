7affa5bd779db0eb4df1d515562a7f1723e06a77
scDjanGo <155238177+scDjanGo@users.noreply.github.com>
2024-11-22 19:22:02 +0600

    щл

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Crafting Digital Experiences: My Portfolio",
   description: "Explore a collection of projects showcasing my expertise in creating modern, functional, and visually appealing web solutions.",
+  keywords: "asadbek-kasymov, asadbek kasymov, portfolio, касымов асадбек, асадбек касымов, Асадбек Касымов портфолио "
 };
 
 export default function RootLayout({
