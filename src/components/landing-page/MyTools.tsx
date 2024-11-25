576ffb4d5e3edd4870b8b301226e50ff3e491dca
scDjanGo <155238177+scDjanGo@users.noreply.github.com>
2024-11-25 17:09:25 +0600

    ok

--- src/components/landing-page/MyTools.tsx
+++ src/components/landing-page/MyTools.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function MyTools() {
     return (
         <div id="myTools" className="flex flex-col items-center gap-[25px] justify-between bg-[#F0F1F3] p-[150px_300px] 1560:p-[100px_30px]">
-            <h2 className="text-center text-[64px] font-[700] 795:text-[32px] pt-[24px] px-[100px] 1240:px-[15px]">
+            <h2 className="text-[#132238] text-center text-[64px] font-[700] 795:text-[32px] pt-[24px] px-[100px] 1240:px-[15px]">
                 My Tools
             </h2>
             <div className="w-full grid grid-cols-[repeat(auto-fit,_minmax(90px,_130px))] gap-[25px] 795:grid-cols-[repeat(auto-fit,_minmax(80px,_90px))] justify-center px-[100px] py-[25px] 1240:px-[15px]">
