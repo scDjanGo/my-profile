2f8a25c976fcaae6a4e3eb9a6e89dde9fbefee5c
scDjanGo <scdjango229101@gmail.com>
2025-01-19 20:47:46 +0600

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // trailingSlash: true,
-  // basePath: '',
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
 };
 
 export default nextConfig;
